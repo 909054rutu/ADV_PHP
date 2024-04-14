9c93983b29d45213d5a2f1c0ae6102339f77f67a
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-04-14 16:49:01 +0530

    Update stud.php

--- stud.php
+++ stud.php
@@ -1,18 +1,18 @@
 <?php
-$con=mysqli_connect("localhost","root","");
-if(!$con)
+$con=mysql_connect("localhost","root","");
+if($con==false)
 {
 die("error");
 }
 $r1=$_POST["t1"];
-mysqli_database($con,"ajax");
-$res=mysqli_qury($con,"select*from stud where std='$r1'");
-while($row=mysqli_fetch_array($res))
+mysql_query("use ajax");
+$res=mysql_query("select*from stud where std='$r1'");
+while($row=mysql_fetch_array($res))
 {
 
 echo("name=".$row[0]);
 echo("per=".$row[1]);
 echo("std=".$row[2]);
 }
-$con->close();
+mysql_close($con);
 ?>
